ed5bb5590f3b07e1d71a69950ba118cb09b63b49
isaac kapela <124640429+isaac-kapela@users.noreply.github.com>
2024-06-12 15:53:03 -0300

    outos bõtões e add estilo

--- Convite-azael/src/components/Convite/Convite.jsx
+++ Convite-azael/src/components/Convite/Convite.jsx
@@ -12,8 +12,9 @@ export default function Convite() {
               Presença
             </button>
             <button>
+              <a href="https://www.google.com/maps/place/Av.+Frederick+Hoffmann,+239+-+Jardim+Coimbra,+S%C3%A3o+Paulo+-+SP,+03693-040/@-23.5299714,-46.4782564,17z/data=!3m1!4b1!4m6!3m5!1s0x94ce60d5799a5977:0x5f410c31c2f00003!8m2!3d-23.5299714!4d-46.4782564!16s%2Fg%2F11c5mk2vjn?entry=ttu" target="_blank">
               Local Da <br />
-              Festa
+              Festa </a>
             </button>
             <button>
               Sugestão De <br />
--- Convite-azael/src/components/Convite/Convite_style.jsx
+++ Convite-azael/src/components/Convite/Convite_style.jsx
@@ -1,29 +1,52 @@
-// Convite_style.js
-import styled from 'styled-components';
+import styled from "styled-components";
+import imgConvite from "../../assets/img-convite.jpg";
 
 export const SectionConvite = styled.section`
   display: flex;
-  justify-content: center; 
+  justify-content: center;
   align-items: center;
   height: 100vh;
+  background-image: url(${imgConvite}); 
+  background-size: cover; 
+  background-position: center; 
+  background-repeat: no-repeat; 
+
+    a {
+  text-decoration: none; 
+  color: inherit; 
+  list-style: none;
+}
+
 
   .botoes {
-    top: 50%;
     display: flex;
-    flex-direction: column; 
+    flex-direction: column;
     align-items: center;
   }
 
   button {
-    margin-bottom: 20px; 
-    height: 100px; 
+    margin-bottom: 20px;
+    height: 100px;
     width: 200px;
-    background-color: #f5f5f5;
+    background-color: black;
     color: purple;
-    font-size: 1.5rem;
+    font-size: 1.3rem;
     font-weight: bold;
     border: none;
     cursor: pointer;
     transition: 0.5s;
+    z-index: 2;
+    padding: 1rem 2rem;
+    border-radius: 50px;
+    box-shadow: 18px 15px 17px 1px rgba(235, 226, 226, 0.52);
+    -webkit-box-shadow: 18px 15px 17px 1px rgba(235, 226, 226, 0.52);
+    -moz-box-shadow: 18px 15px 17px 1px rgba(235, 226, 226, 0.52);
+    outline: 2px solid transparent; 
+  }
+
+  button:active {
+    transform: scale(0.95); 
+    outline-color: purple; 
+    outline-width: 15px;
   }
 `;
